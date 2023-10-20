9d473ff6cefbee3613f34870b8d5b0c135dc5010
BrevenBennett <Breven.stars@gmail.com>
2023-10-20 11:49:23 -0500

    finished the react crash course

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,10 +1,31 @@
-import { useParams } from "react-router-dom"
-import React, { useEffect } from "react"
+import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Users() {
-    const { username } = useParams()
+  const { id } = useParams();
+  const [user, setUser] = useState({});
 
-    return <h1>{username}</h1>
+  async function fetchUser() {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    setUser(data);
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <div>
+      <Link to="/">Go Back</Link>
+      <p>{user.id}</p>
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <p>{user.username}</p>
+    </div>
+  );
 }
 
-export default Users 
+export default Users;
